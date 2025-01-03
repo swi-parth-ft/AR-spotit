ea4c4cc11a130112a31e6d9fa56bbf803085b6c3
Parth Antala <theparthantala@gmail.com>
2025-01-03 15:09:06 -0500

    Initial Commit

---
+++ AR spotit/AR_spotitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_spotitApp.swift
+//  AR spotit
+//
+//  Created by Parth Antala on 2025-01-03.
+//
+
+import SwiftUI
+
+@main
+struct AR_spotitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR spotit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AR spotit
+//
+//  Created by Parth Antala on 2025-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
